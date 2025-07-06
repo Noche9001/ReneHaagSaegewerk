4ac70117adc60320dd5cc31200bfbfd697d13f11
Noche9001 <noah.dambacher@gmx.de>
2025-07-06 21:39:17 +0200

    Project setup

---
+++ backend/src/test/java/com/renehaagsaegewerk/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renehaagsaegewerk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
