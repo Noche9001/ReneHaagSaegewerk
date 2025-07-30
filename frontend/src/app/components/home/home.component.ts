f34682918cef358994d9cb9fa357cebb2d48e8c0
Noche9001 <noah.dambacher@gmx.de>
2025-07-30 21:27:44 +0200

    Footer component

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationService } from '../../service/navigation.service';
 import { NavbarComponent } from '../general/navbar/navbar.component';
+import { FooterComponent } from '../general/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
