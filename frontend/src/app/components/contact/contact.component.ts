8aeca82977027a76230bc0c1a5468faa99e7e0e5
Noche9001 <noah.dambacher@gmx.de>
2025-08-06 14:56:18 +0200

    Contact Page, Fontawesome

--- frontend/src/app/components/contact/contact.component.ts
+++ frontend/src/app/components/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { NavigationService } from '../../service/navigation.service';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-contact.component',
-  imports: [ContactFormComponent],
+  imports: [ContactFormComponent, NgOptimizedImage],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
